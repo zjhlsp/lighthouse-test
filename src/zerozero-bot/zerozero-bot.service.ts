6173d8878c2289c14ff9a11668f0dd241bc98afa
zoujiahao <zoujiahao@bolean.com.cn>
2024-09-13 15:01:02 +0800

    feat✨: 推送服务逻辑优化

--- src/zerozero-bot/zerozero-bot.service.ts
+++ src/zerozero-bot/zerozero-bot.service.ts
@@ -9,99 +9,77 @@ function formatCommits(commits) {
     branch: commits.ref.split('/').pop(),
     name: commits.repository.name,
     url: commits.repository.url,
-    commits: commits.commits.map((item) => {
-      return {
-        id: item.id,
-        timestamp: item.timestamp,
-        message: item.message,
-        author: item.committer.name,
-      };
-    }),
+    commits: commits.commits
+      .map((item) => {
+        return {
+          id: item.id,
+          timestamp: item.timestamp,
+          message: item.message,
+          author: item.committer.name,
+        };
+      })
+      .filter(
+        (item) =>
+          item.message.includes('Merge pull request') ||
+          item.message.includes('Update from Shopify'),
+      ),
   };
   return message;
+}
+// 过滤条件
+function filterCommits(message) {
+  const branch = ['main', 'test', 'master'];
+  return branch.includes(message.branch);
 }
 
 // 发送消息到飞书
 async function sendToFeishu(message) {
-  try {
-    if (
-      !(
-        message.branch === 'main' ||
-        message.branch === 'test' ||
-        message.branch === 'master'
-      )
-    ) {
-      return;
-    }
-    const response = await fetch(WEBHOOK_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        msg_type: 'post',
-        content: {
-          post: {
-            zh_cn: {
-              title: `⭐⭐ ${message.name} -> ${message.branch} 更新 ⭐⭐`,
-              content: message.commits.map((item) => {
-                return [
-                  {
-                    tag: 'text',
-                    text: `  - 更新内容: ${item.message} \n`,
-                  },
-                  {
-                    tag: 'text',
-                    text: `  - 时间: ${item.timestamp} \n`,
-                  },
-                  {
-                    tag: 'text',
-                    text: `  - commit id: ${item.id} \n \n`,
-                  },
-                  {
-                    tag: 'text',
-                    text: `----------------------------------------- \n \n`,
-                  },
-                ];
-              }),
-
-              // content: [
-              //   [
-              //     {
-              //       tag: 'text',
-              //       text: `  - 更新内容: ${message} \n`,
-              //     },
-              //     // {
-              //     //   tag: 'text',
-              //     //   text: '  - Commit 地址: ',
-              //     //   href: message.url,
-              //     // },
-              //     // {
-              //     //   tag: 'a',
-              //     //   text: '请点击 \n',
-              //     //   href: message.url,
-              //     // },
-              //   ],
-              // ],
+  if (filterCommits(message)) {
+    try {
+      const response = await fetch(WEBHOOK_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          msg_type: 'post',
+          content: {
+            post: {
+              zh_cn: {
+                title:
+                  message.branch === 'test'
+                    ? `✨✨${message.name} 测试服更新✨✨`
+                    : `🎊🎊${message.name} 正式服更新🎊🎊`,
+                content: message.commits.map((item) => {
+                  return [
+                    {
+                      tag: 'text',
+                      text: `🥇更新内容🥇: ${item.message} \n`,
+                    },
+                    {
+                      tag: 'text',
+                      text: `🥈时间🥈: ${item.timestamp} \n`,
+                    },
+                    {
+                      tag: 'text',
+                      text: `------------------------- \n`,
+                    },
+                  ];
+                }),
+              },
             },
           },
-        },
-      }),
-      // body: JSON.stringify({
-      //   msg_type: 'text',
-      //   content: {
-      //     text: message,
-      //   },
-      // }),
-    });
+        }),
+      });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      console.log('消息发送成功!');
+    } catch (error) {
+      console.error(`消息发送失败! 错误: ${error}`);
     }
-
-    console.log('消息发送成功!');
-  } catch (error) {
-    console.error(`消息发送失败! 错误: ${error}`);
   }
 }
 
